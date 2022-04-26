4223fbe215b3c33d8dda954467e302014c75ce49
vladislav_p <vladislav_p@maximail.org>
2022-04-26 09:03:26 +0500

    Add consumer template

--- src/consumer.php
+++ src/consumer.php
@@ -1,1 +1,46 @@
 <?php
+
+declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$db = new PDO('mysql:host=localhost;dbname=;charset=utf8mb4', 'login', 'password');
+while (true) {
+    try {
+        $queue = 'queue';
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        $channel->queue_declare($queue, false, false, false, false);
+        $channel->basic_qos(null, 1 , null);
+
+        $callback = function ($msg) {
+            $array = json_decode($msg->body, true);
+            extract($array);
+
+        };
+
+        $channel->basic_consume($queue, '', false, true, false, false, $callback);
+        /**
+         * @param \PhpAmqpLib\Channel\AMQPChannel $channel
+         * @param \PhpAmqpLib\Connection\AbstractConnection $connection
+         */
+        function shutdown($channel, $connection)
+        {
+            $channel->close();
+            $connection->close();
+        }
+
+        register_shutdown_function('shutdown', $channel, $connection);
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+    } catch (\Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+        sleep(1);
+    }
+}
+
+
